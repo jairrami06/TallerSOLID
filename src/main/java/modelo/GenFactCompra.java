19b771544ce3854cb720d4bf360d7070dc355fc1
jairrami06 <117552816+jairrami06@users.noreply.github.com>
2023-06-08 18:02:43 -0500

    Aplicación principios SOLID
    Clases e interfaces desarrolladas una vez aplicados los principios SOLID en el código dado.

---
+++ src/main/java/modelo/GenFactCompra.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author jaira
+ */
+public class GenFactCompra {
+    public Factura generarFactura(){
+        //generar nueva factura de la compra
+        return null;
+    }
+
+}
